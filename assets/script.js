b23f3042a1d3fdb720b82b8272ff250a442cd48e
JeanneGbn <j.goubin@gmail.com>
2023-05-10 10:56:36 +0200

    Avec carousel images et tagLine actif

--- assets/script.js
+++ assets/script.js
@@ -20,17 +20,25 @@ const slides = [
 const bannerImg = document.querySelector('.banner-img')
 let currentSlide = 0
 
+const tagLine = document.querySelector('.banner-tagline')
+let currentDots = 0
+console.log(tagLine)
+
 const leftArrow = document.querySelector(".arrow_left")
 leftArrow.addEventListener('click', function (){
-	currentSlide = currentSlide-1
+	 currentSlide = currentSlide-1
 	bannerImg.src="assets/images/slideshow/"+slides[currentSlide].image
+	tagLine.innerHTML=slides[currentSlide].tagLine
 })
 
 const rightArrow = document.querySelector(".arrow_right")
 rightArrow.addEventListener('click', function (){
 	currentSlide = currentSlide+1
 	bannerImg.src="assets/images/slideshow/"+slides[currentSlide].image
+	tagLine.innerHTML=slides[currentSlide].tagLine
 })
+
+
 
 const nbSlides = slides.length
 console.log(nbSlides)
@@ -45,3 +53,44 @@ for(let i = 0; i < nbSlides; i++){
 		dots.innerHTML += '<div class="dot"> </div>'
 	}
 }
+/*
+document.querySelector('.dots');
+    let elements = Array.from(container.children);
+    console.log(elements);
+    let dotdot = click ;
+    let dotselect = elements[dotdot];
+    console.log(dotselect);
+    dotselect.classList.add('dot_selected');
+
+
+function bulletsBack(){
+	currentDots = currentDots-1
+	dots[currentDots] += '<div class="dot dot_selected"> </div>'
+}
+
+function bulletsNext(){
+	currentDots = currentDots+1
+	dots[currentDots] += '<div class="dot dot_selected"> </div>'
+}
+
+
+
+
+document.querySelector('.dots');
+    let elements = Array.from(container.children);
+    console.log(elements);
+    let dotdot = click ;
+    let dotselect = elements[dotdot];
+    console.log(dotselect);
+    dotselect.classList.add('dot_selected');
+
+
+let dotadd = click ;
+    let dotrm =click -1;
+    let dotselect = elements[dotadd];
+    let dotremove =elements[dotrm];
+    console.log(dotselect);
+    dotselect.classList.add('dot_selected');
+    dotremove.classList.remove('dot_selected');
+
+*/
